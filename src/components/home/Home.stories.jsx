dc35ef7276fba8947f14a900f348cdf656d3c2c1
jezpo <jezocam@gmail.com>
2022-07-10 19:46:03 +0000

    practica3

---
+++ src/components/home/Home.stories.jsx
@@ -0,0 +1,9 @@
+import React from "react"; 
+import Home from "./Home"; 
+export default { 
+  title: "EdgarOcampoOcampo/app/Home", 
+  component: Home, 
+}; 
+ 
+const Template = (args) => <Home {...args} />; 
+export const home= Template.bind({});
